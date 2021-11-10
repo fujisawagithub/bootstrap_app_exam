88acea30ac5335e11ed6cc01ec2e64c8e24ae472
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-10 11:52:23 +0900

    コメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
-Rails.application.routes.draw do
+=begin
+rescue => exception
+  
+end#Rails.application.routes.draw do
   resources :posts do
     collection do
       post :confirm
     end
   end
+end
+=end
+Rails.application.routes.draw do
+  root to: 'posts#index'  <!-- こちらを追記します。 -->
+  resources :blogs
 end
